4e83b8e4fd33c31def2a5cc80cbfde68dad646fa
Vilvi <tradegyzx@gmail.com>
2022-11-18 18:24:52 +0100

    Added support for multiple formulas

--- geometry/geometry/Program.cs
+++ geometry/geometry/Program.cs
@@ -6,16 +6,23 @@ internal class Program
     {
         bool kørende = true;
         double pi, radius, resultat;
+        char valg, formel;
         pi = Math.PI;
 
         while (kørende)
         {
-            Console.Write("Hvad er radius? ");
-            radius = Convert.ToDouble(Console.ReadLine());
-            resultat = 2 * pi * radius;
-            Console.WriteLine("Omkreds er: " + resultat);
-            Console.Write("Vil du køre igen? j/n ");
-            char valg = Convert.ToChar(Console.ReadLine());
+            Console.Write("Vælg en formel: a=areal, o=omkreds, d=diameter ");
+            formel = Convert.ToChar(Console.ReadLine());
+            if (formel == 'o')
+            {
+                Console.Write("Hvad er radius? ");
+                radius = Convert.ToDouble(Console.ReadLine());
+                resultat = 2 * pi * radius;
+                Console.WriteLine("Omkreds er: " + resultat);
+
+            }
+                Console.Write("Vil du køre igen? j/n ");
+                valg = Convert.ToChar(Console.ReadLine());
             if (valg == 'j')
             {
                 kørende = true;
