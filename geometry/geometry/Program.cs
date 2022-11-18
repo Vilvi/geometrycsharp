5022e3ee1d73136212bb6cc3db57977613f4ab1a
Vilvi <tradegyzx@gmail.com>
2022-11-18 17:36:38 +0100

    Created the project and started it.

---
+++ geometry/geometry/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool kørende = true;
+        double pi, radius, resultat;
+        pi = Math.PI;
+
+        while (kørende)
+        {
+            Console.Write("Hvad er radius? ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            resultat = 2 * pi * radius;
+            Console.WriteLine("Omkreds er: " + resultat);
+            Console.Write("Vil du køre igen? j/n ");
+            char valg = Convert.ToChar(Console.ReadLine());
+            if (valg == 'j')
+            {
+                kørende = true;
+            }
+            else
+            {
+                kørende = false;
+            }
+        }
+    }
+}
