ee92c29f8990d061d321576a56d55bf12c8f28dc
Vilvi <tradegyzx@gmail.com>
2022-11-19 15:09:22 +0100

    Added methods to run repeated code

--- geometry/geometry/Program.cs
+++ geometry/geometry/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 internal class Program
 {
@@ -13,25 +14,16 @@ internal class Program
 
         while (running)
         {
-            // User chooses which formula for the chosen shape they want to use.
-            Console.Write("Choose a formula c = circumference a = area ");
-            // Saves the user choice in a variable called "formula", and converts it to a char to save space, since the input is always going to be 1 character.
-            formula = Convert.ToChar(Console.ReadLine());
+            formula = AskFormula();
             if (formula == 'c')
             {
-                // Asks for the radius and puts it into a circumference formula and then outputs the result.
-                Console.Write("What's the radius? ");
-                radius = Convert.ToDouble(Console.ReadLine());
-                result = 2 * pi * radius;
-                Console.WriteLine("Circumference is: " + result);
+                radius = AskForRadius();
+                Console.WriteLine("Circumference is: " + CalculateCircumference(radius));
 
             } else if (formula == 'a')
             {
-                // Asks for the radius and puts it into a area formula and then outputs the result.
-                Console.Write("What's the radius? ");
-                radius = Convert.ToDouble(Console.ReadLine());
-                result = pi * Math.Pow(radius, 2);
-                Console.WriteLine("Area is: " + result);
+                radius = AskForRadius();
+                Console.WriteLine("Area is: " + CalculateArea(radius));
             }
             // Ask the user if they want to run again. 'y' changes the running boolean to true to keep the program running.
             Console.Write("Do you want to run again? y/n ");
@@ -44,6 +36,28 @@ internal class Program
             {
                 running = false;
             }
+        }
+        // Calculates the circumference when given the radius.
+        double CalculateCircumference(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+        // Calculates the area when given the radius.
+        double CalculateArea(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        // Asks the user what the radius is and returns it.
+        double AskForRadius()
+        {
+            Console.Write("What's the radius? ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+        // Asks for the which formula the user wishes to calculate in the given shape.
+        char AskFormula()
+        {
+            Console.Write("Choose a formula c = circumference a = area ");
+            return Convert.ToChar(Console.ReadLine());
         }
     }
 }
